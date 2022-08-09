3067092ea6befc22472cd59c4a8fd49039abae15
Victor C <victorcasrod11@gmail.com>
2022-08-09 17:47:56 -0500

    Se implemento el login y su validacion

--- js/demo/validacion.js
+++ js/demo/validacion.js
@@ -1,34 +1,22 @@
-var emailJson = []
+function validate(){
 
-async function validate(){
-
-    await getData()
+ var logeo=false;
     var userName = document.getElementById("exampleInputEmail").value
     var userPass = document.getElementById("exampleInputPassword").value
-    console.log(userName)
-    console.log(userPass)
-    for (i = 0; i < emailJson.length; i++){
-        console.log("Entre al for")
-        if(userName == emailJson[i] && userPass == emailJson[i]){
-            console.log("Entro al if")
-
-           // return location.href = "../../index.html"
-        }
+    var user = ["vcastro@gmail.com", 123456]
+           
+    if(userName == user[0] && userPass == user[1]){
+        console.log("validado");
+        logeo=true;
+        
+    window.location.href="index.html"
+   
+          
+    }else{
+        alert("Nombre de usuario o Contraseña incorrectos")
     }
-    alert("Nombre de usuario o Contraseña incorrectos")
-}
+    console.log(logeo)
+return logeo
 
-async function getData(){
-    const apiUrl = "https://node-api-iot.herokuapp.com/users"
-
-    const response = await fetch(apiUrl)
-    const userData = await response.json()
-
-    const email = userData.map( (x) => x.email)
-
-    emailJson = email
-
-    console.log("Ya tengo los datos de la API")
-    console.log(emailJson)
-
-}
+    
+} 
